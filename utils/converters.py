84adf808321144f4f577fe312de2f153a2b7dac6
aiulukbek <ai.ulukbek.attokurov@gmail.com>
2025-04-06 19:09:07 +0600

    Docstrings are added.

--- utils/converters.py
+++ utils/converters.py
@@ -1,15 +1,30 @@
 
+# helper functions for formatting and date conversions 
 import numpy as np 
 import pandas as pd 
 from typing import List 
 
 
 def convert_to_list(items : List[np.int64]) -> List[int]: 
+    """
+        converts list of numpy into list of ints 
+        Params:
+            :param items - the sequence of numpy arrays 
+        Returns: 
+            a list of integers 
+    """
     new_items = [item.item() for item in items] 
     return new_items 
 
 
 def timestamp_to_datetime(timestamp: pd.Series ) -> pd.Timestamp: 
+    """
+        converts unixtimestamp into datetime 
+        Params: 
+            :param timestamp - unixtimestamp 
+        Returns: 
+            datetime 
+    """
     try:
         timestamp_date = pd.to_datetime(timestamp, unit = 'ms')
     except Exception as e: 
@@ -18,6 +33,13 @@ def timestamp_to_datetime(timestamp: pd.Series ) -> pd.Timestamp:
     return timestamp_date
 
 def datetime_to_date(dt: pd.Series) -> pd.Series: 
+    """
+        converts datetime into date 
+        Params: 
+            :param dt - datetime 
+        Returns: 
+            a series in date format 
+    """
     values = np.all(dt.apply(lambda item: isinstance(item, pd.Timestamp)).values)
 
     if not values:
@@ -25,6 +47,13 @@ def datetime_to_date(dt: pd.Series) -> pd.Series:
     return dt.dt.date
 
 def format_date(events : pd.DataFrame) -> pd.DataFrame: 
+    """
+        formats event timestamp into proper date formats 
+        Params: 
+            :param events - events dataframe 
+        Returns: 
+            events with proper date formats 
+    """
     events['event_datetime'] = timestamp_to_datetime(events.timestamp)
     events['events_date'] = datetime_to_date(events.event_datetime)
     return events
